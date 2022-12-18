3f94ead2ffde15ee7804a2c7ba8afe4acb918aa4
Sudip Adhikari <31766056+sudipadhikari01@users.noreply.github.com>
2022-12-18 14:03:17 -0600

    file modified

--- src/main/java/com/example/springcicddemo/controller/HelloController.java
+++ src/main/java/com/example/springcicddemo/controller/HelloController.java
@@ -1,10 +1,14 @@
 package com.example.springcicddemo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class HelloController {
 
+    @GetMapping("/hello")
     public String welcome(){
         return "Welcome to Hello Controller";
     }
